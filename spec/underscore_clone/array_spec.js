43bfaa7efdbf1c85606d2d60d03217b009406eef
Drew Samsen <drew@weblogicnow.com>
2014-02-22 14:22:31 -0800

    First failing spec for #each method

--- spec/underscore_clone/array_spec.js
+++ spec/underscore_clone/array_spec.js
@@ -1,1 +1,13 @@
 // Array specs
+
+describe("_.each", function() {
+  it("iterates over an array calling a function for each item", function() {
+    var arr = [1, 2, 3],
+        memo = [],
+        iterator = function(value) { memo.push(value); };
+
+    _.each(arr, iterator);
+
+    expect(memo).toEqual([1, 2, 3]);
+  });
+});
